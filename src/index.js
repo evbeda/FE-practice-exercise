eb961e16c9a4ada16153c015b1e7c9a952c6ca4d
Mario Merida <mamerida2018@gmail.com>
2022-09-06 09:35:45 -0300

    structure and use react redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import App from "./components/App";
+import { Provider } from "react-redux";
+import { reducerSample as rootReducer } from "../src/reducer/index";
+import { createStore, applyMiddleware} from "redux";
+import thunk from "redux-thunk";
+export const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   rootElement
 );
