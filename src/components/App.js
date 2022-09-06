2af77bc9dc647da1062d60e4ba43f2ec5cca370f
Matias Bertani <matiasbertani@gmail.com>
2022-09-06 10:23:01 -0300

    creating the card componentsç

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,45 @@
 import React from "react";
 import "../styles.css";
+
 import ContainerConected from './Container/ContainerConected.jsx';
+
+
+
+import { Card } from "./card/Card";
+
+const evntoPrueba = {
+  "id": 1,
+  "eventName": "Test Event 1",
+  "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eleifend consequat velit, at rutrum lacus semper vel. Nam tempus vulputate sapien eget facilisis. Nulla ut.",
+  "imageUrl": "https://place-hold.it/200x200",
+  "startDatetime": "2022-10-15T13:45:00Z",
+  "endDateTime": "2022-10-15T16:00:00Z",
+  "venue": "123 Fake St.",
+  "followers": 15698,
+  "organizer": "Pepe Guilletes",
+  "tickets": [
+    {
+      "quantity": 100,
+      "price": 14.56,
+      "name": "General Ticket",
+      "fee_type": "fixed",
+      "fee": "1.50"
+    },
+    {
+      "quantity": 50,
+      "price": 35.0,
+      "name": "VIP Ticket",
+      "fee_type": "percent",
+      "fee": "5"
+    },
+    {
+      "quantity": 5,
+      "price": 35.0,
+      "name": "VIP Ticket",
+      "fee_type": "none"
+    }
+  ]
+}
 
 const App = () => {
   return (
@@ -9,5 +48,16 @@ const App = () => {
     </div>
   );
 };
+// const App = () => {
+//   return (
+//     <>
+//       <div className="App">
+//       Test page
+//       </div>
+//       <Card event={evntoPrueba}/>
+//     </>
+// );
+// };
+
 
 export default App;
---
+++ src/components/subcontent/Subcontent.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { FollowersIcon } from "../followerIcon/FollowersIcon";
+import './Subcontent.css'
+
+const priceInfo = (tickets) => {
+  return `A partir de $ ${minimumPrice(tickets)}`
+}
+
+const minimumPrice = (tickets) => {
+  let chepeastTocket = tickets.reduce(
+    (previus, current) => {
+      return (current.price > previus.price
+        && current.quantity > 0) ? current : previus
+    })
+  return chepeastTocket.price
+}
+
+export const Subcontent = ({tickets, venue, organizer, followers}) => {
+  return (
+
+    <div className="eds-event-card-content__sub-content">
+      <div className="eds-event-card-content__sub eds-text-bm eds-text-color--ui-600 eds-l-mar-top-1">
+        <div data-subcontent-key="location" className="card-text--truncated__one">
+          {venue}
+        </div>
+      </div>
+
+      {/*  PRECIO  */}
+      <div className="eds-event-card-content__sub eds-text-bm eds-text-color--ui-600 eds-l-mar-top-1">
+        {priceInfo(tickets)}
+      </div>
+
+      <div className="eds-event-card-content__sub eds-text-bm eds-text-color--ui-600 eds-l-mar-top-1">
+        <div data-subcontent-key="follow" className="eds-event-card__sub-content--signal eds-text-color--ui-800 eds-text-weight--heavy">
+
+          {/*  ORGANIZER  */}
+          <div data-subcontent-key="organizerName" className="eds-event-card__sub-content--organizer eds-text-color--ui-800 eds-text-weight--heavy card-text--truncated__two">
+            {organizer}
+          </div>
+
+          {/*  ICON PERSON  */}
+          <FollowersIcon />
+
+          {/* followers*/}
+          <span className="eds-l-pad-hor-1">
+          </span>{followers} seguidores
+        </div>
+      </div>
+    </div>
+
+  );  
+};
