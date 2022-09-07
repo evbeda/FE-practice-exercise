02b1e8c1e23a8aea16f250c2471f6bab4049e522
Mario Merida <mamerida2018@gmail.com>
2022-09-07 14:39:32 -0300

    fix date and add images

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,26 @@
+import moment from "moment";
+import 'moment/locale/es';
+moment.locale('es');
+
+
+export const formatedDatetime = (datetime) => {
+  let eventDateToShare = ""
+  let eventDate = moment(datetime)
+  let today = moment()
+  const diffDates = eventDate.diff(today,"days")
+  switch(diffDates){
+    case 0:
+      eventDateToShare = "Today!!!"
+      break;
+    case 1:
+      eventDateToShare = "Tomorrow!!!"
+      break;
+    default:
+      eventDateToShare = eventDate.format("llll")
+  }
+  return eventDateToShare
+}
+
 export function convertToArray(number){
     let numberArray = [];
     for (let i = 0; i < number + 1; i++){
