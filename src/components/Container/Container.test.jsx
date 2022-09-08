b4233b50048192144ee19b041de34370a7b15666
Mario Merida <mamerida2018@gmail.com>
2022-09-08 17:03:21 -0300

    check container component

--- src/components/Container/Container.test.jsx
+++ src/components/Container/Container.test.jsx
@@ -1,7 +1,40 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import {Container} from './Container';
+import '@testing-library/jest-dom';
 
 
 jest.mock('../Card/Card', () => ({
     Card: () => (<div data-testid="card-test" />)
 }));
+
+jest.mock('../Modal/Modal', () => ({
+    Modal: () => (<div data-testid="modal-test" />)
+}));
+
+describe("Container page", ()=>{
+    const testFunction = async()=>{
+        return {payload:[{id:1}]}
+    }
+
+    it("header render",()=>{
+        render(<Container eventStorage={[]} getAllEvents={testFunction} />)
+        expect(screen.getByTestId("header")).toBeInTheDocument();
+    })
+    it("title render",()=>{
+        render(<Container eventStorage={[]} getAllEvents={testFunction} />)
+        expect(screen.getByTestId("events-title")).toBeInTheDocument();
+    })
+    it("imagen render",()=>{
+        render(<Container eventStorage={[]} getAllEvents={testFunction} />)
+        expect(screen.getByTestId("home-image")).toBeInTheDocument();
+    })
+    it("svg render",()=>{
+        render(<Container eventStorage={[]} getAllEvents={testFunction} />)
+        expect(screen.getByTestId("svg")).toBeInTheDocument();
+    })
+    it("show cards",()=>{
+        render(<Container eventStorage={[{test1:1}]} getAllEvents={testFunction} />)
+        expect(screen.getByTestId("card-test")).toBeInTheDocument();
+    })
+    //como probar que el state show modal se active y muestre el modal
+})
